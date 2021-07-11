beb82ea3347dc6024f9b0cab5c6fceda2c079045
Conor Kennedy <conor@conorkennedy.com>
2021-07-10 20:05:31 -0400

    backgrounds WIP

--- src/dnd/Background/Soldier.java
+++ src/dnd/Background/Soldier.java
@@ -1,26 +1,19 @@
 package dnd.Background;
 
-public class Acolyte implements Background{
-
-    private String backgroundName = "Acolyte";
-    private String skillProficiencies[] = {"Insight", "Religion"};
-    private String equipment[] = {"A holy symbol", "Prayer book or wheel", "5 Sticks of incenses", "vestments", "A pouch containing 15 gp"};
-
-    //todo fill this out
-
+public class Soldier implements Background{
     @Override
     public String getBackgroundName() {
-        return backgroundName;
+        return null;
     }
 
     @Override
     public String[] getSkillProficiencies() {
-        return skillProficiencies;
+        return new String[0];
     }
 
     @Override
     public String[] getEquipment() {
-        return equipment;
+        return new String[0];
     }
 
     @Override
